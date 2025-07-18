b37f78391dbc4777ca0a00eefa54f2876efee9b6
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-07-18 23:31:06 +0200

    added instantiation of nios2 component from platform designer

--- customTimerPeripheral.sv
+++ customTimerPeripheral.sv
@@ -41,5 +41,20 @@ module customTimerPeripheral (
             readdata = 32'd0;  // Default when not reading
         end
     end
+	 
+	 nios2_qsys_0 u0 (
+    .clk_clk        (clk),         // Connect to your system clock
+    .reset_reset    (reset),       // Connect to system reset
+    .custom_timer_0_clock (clk),        // if your timer has a separate clock
+    .custom_timer_0_reset (reset),    // and a reset
+    .custom_timer_0_timer_mm_slave_address    (address),
+    .custom_timer_0_timer_mm_slave_read       (read),
+    .custom_timer_0_timer_mm_slave_readdata   (readdata),
+    .custom_timer_0_timer_mm_slave_write      (write),
+    .custom_timer_0_timer_mm_slave_writedata  (writedata)
+    // other signals like JTAG UART, instruction/data masters, etc.
+	);
+
 
 endmodule
+
